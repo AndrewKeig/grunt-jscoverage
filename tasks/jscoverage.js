12c1a46590bba43954bf6f5b47dbd355ba0f078f
m1kc (Max Musatov) <m1kc@yandex.ru>
2014-06-12 16:04:17 +0400

    More proper jscoverage 0.5.x support.

--- tasks/jscoverage.js
+++ tasks/jscoverage.js
@@ -4,10 +4,9 @@ module.exports = function(grunt) {
   grunt.registerMultiTask("jscoverage", "Grunt task for jscoverage; which will parse your source code and generate an instrumented version allowing testing tools to generate code coverage reports", function() {
     var jscoverage = require('jscoverage');
     var options = this.options();
-    var exclude = options.exclude || [];
     this.files.forEach(function(file){
       file.src.forEach(function(source){
-        jscoverage.processFile(source, file.dest, exclude, options);
+        jscoverage.processFile(source, file.dest, options);
       });
     });
   });
